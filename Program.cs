49402aae541a8e8e29884b5f22d13ba78471dd4d
Daniel Morrison <dan@morrison.ph>
2023-05-14 17:46:38 -0400

    Cleaner code, but slight regression

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using CoreOSC.Types;
 using System.Net.Sockets;
 
-
 if (args.Length != 2) {
     args = new string[] {"set", "test.txt"};
 }
@@ -10,32 +9,29 @@ if (args.Length != 2) {
 var setMode = args[0] == "set";
 var fileName = args[1];
 
-var messages = new List<Task<Byte[]>>();
-if (setMode) {
-    BytesConverter BytesConverter = new BytesConverter();
-    OscMessageConverter MessageConverter = new OscMessageConverter();
-    foreach (var line in File.ReadAllLines(fileName).ToList()) {
-        messages.Add(Task.Run(() => {
-            var data = line.Split(".");
-            var value = string.Join('.', data.Skip(2));
-            var message = new OscMessage(new Address($"/{data[0]}/{data[1]}"), new object[] { value });
-            var dWords = MessageConverter.Serialize(message);
-            _ = BytesConverter.Deserialize(dWords, out var bytes);
-            return bytes.ToArray();
-        }));
-    }
-}
-
+BytesConverter BytesConverter = new BytesConverter();
+OscMessageConverter MessageConverter = new OscMessageConverter();
 
 using (var udpClient = new UdpClient("192.168.2.41", 2223)) {
-    BytesConverter BytesConverter = new BytesConverter();
+    byte[] ConvertMessage(string line) {
+        var data = line.Split(".");
+        var value = string.Join('.', data.Skip(2));
+        var message = new OscMessage(new Address($"{data[0]}/{data[1]}"), new object[] { value });
+        var dWords = MessageConverter.Serialize(message);
+        _ = BytesConverter.Deserialize(dWords, out var bytes);
+        return bytes.ToArray();
+    }
     async Task<IEnumerable<DWord>> Query(byte[] message) {
         await udpClient.SendAsync(message, message.Length);
         var result = await udpClient.ReceiveAsync();
         return BytesConverter.Serialize(result.Buffer);
     }
 
-    foreach (var message in messages) {
-        await Query(await message);
+    if (setMode) {
+        var messages = File.ReadAllLines(fileName)
+            .Select(line => Task.Run(() => ConvertMessage(line)));
+        foreach (var message in messages) {
+            await Query(await message);
+        }
     }
 }
